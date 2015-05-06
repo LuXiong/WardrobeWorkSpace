64c658b8bb4bad30601d2214ded23c6eb0bcfd98
hh <1628401271@qq.com>
2015-05-06 14:53:31 +0800

    WelcomeAdapter

---
+++ PrivateWardrobe/src/com/privatewardrobe/adapter/WelcomeAdapter.java
@@ -0,0 +1,35 @@
+package com.privatewardrobe.adapter;
+
+import java.util.ArrayList;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+public class WelcomeAdapter extends PagerAdapter {
+private ArrayList<View> views;
+	
+	public WelcomeAdapter(ArrayList<View> views){
+		
+		this.views = views;
+	}
+	@Override
+	public int getCount() {
+		return this.views.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View arg0, Object arg1) {
+		return arg0 == arg1;
+	}
+	
+	public void destroyItem(View container, int position, Object object) {
+		((ViewPager)container).removeView(views.get(position));
+	}
+	
+	//Ò³Ãæview
+	public Object instantiateItem(View container, int position) {
+		((ViewPager)container).addView(views.get(position));
+		return views.get(position);
+	}
+}
