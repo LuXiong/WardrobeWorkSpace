442a71fd3a69b8f62a84f45cc77b5ff81caa00ce
hh <13006169407@163.com>
2015-05-12 23:53:32 +0800

    29

---
+++ PrivateWardrobe/src/com/privatewardrobe/activity/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.privatewardrobe.activity;
+
+import android.os.Bundle;
+
+import com.privatewardrobe.R;
+import android.app.Activity;
+
+public class AboutUsActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_aboutus);
+	}
+
+}
