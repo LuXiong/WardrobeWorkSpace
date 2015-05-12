af15c7dcb8dd82d7e8d9981c8c36029a13c45ac2
zyz <1023409822@qq.com>
2015-05-12 22:26:51 +0800

    detail

--- PrivateWardrobe/src/com/privatewardrobe/activity/ClothesDetailActivity.java
+++ PrivateWardrobe/src/com/privatewardrobe/activity/ClothesDetailActivity.java
@@ -1,14 +1,46 @@
 package com.privatewardrobe.activity;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.privatewardrobe.BaseActivity;
 import com.privatewardrobe.R;
+import com.privatewardrobe.model.Suit;
 
 public class ClothesDetailActivity extends BaseActivity{
+	private TextView mDescriptionTextView,mColorTextView,mCategoryView,mExponentTextView;
+	private TextView mCreateTimeTextView,mLastEditTextView,mIsLike;
+	private ImageView mImgImageView;
+	private ListView mSuitListView;
+	
+	private ArrayList<Suit> mSuitList;
 @Override
 protected void onCreate(Bundle bundle) {
 	super.onCreate(bundle);
+	mActionBar = getMyActionBar();
+	mActionBar.setLeftDrawable(null);
 	setContentView(R.layout.activity_clothes_detail);
+	findView();
+	initView();
+}
+private void initView() {
+	// TODO Auto-generated method stub
+	
+}
+private void findView() {
+	// TODO Auto-generated method stub
+	mDescriptionTextView = (TextView)findViewById(R.id.activity_clothes_detail_description);
+	mColorTextView = (TextView)findViewById(R.id.activity_clothes_detail_color);
+	mCategoryView = (TextView)findViewById(R.id.activity_clothes_detail_category);
+	mExponentTextView = (TextView)findViewById(R.id.activity_clothes_detail_exponent);
+	mCreateTimeTextView = (TextView)findViewById(R.id.activity_clothes_detail_createTime);
+	mLastEditTextView = (TextView)findViewById(R.id.activity_clothes_detail_lastEdit);
+	mIsLike = (TextView)findViewById(R.id.activity_clothes_detail_isLike);
+	mImgImageView = (ImageView)findViewById(R.id.activity_clothes_detail_img);
+	mSuitListView = (ListView)findViewById(R.id.activity_clothes_detail_suitList);
 }
 }
--- PrivateWardrobe/src/com/privatewardrobe/activity/SuitDetailActivity.java
+++ PrivateWardrobe/src/com/privatewardrobe/activity/SuitDetailActivity.java
@@ -1,5 +1,45 @@
 package com.privatewardrobe.activity;
 
-public class SuitDetailActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.privatewardrobe.BaseActivity;
+import com.privatewardrobe.R;
+
+public class SuitDetailActivity extends BaseActivity{
+	
+	private TextView mDescriptionTextView,mWeatherTextView,mOccasionTextView;
+	private TextView mCreateTimeTextView,mLastEditTextView,mIsLike;
+	private ImageView mImgImageView;
+	private ListView mClothesListView;
+
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		mActionBar = getMyActionBar();
+		mActionBar.setLeftDrawable(null);
+		setContentView(R.layout.activity_suit_detail);
+		findView();
+		initView();
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void findView() {
+		// TODO Auto-generated method stub
+		mDescriptionTextView = (TextView)findViewById(R.id.activity_suit_detail_description);
+		mWeatherTextView = (TextView)findViewById(R.id.activity_suit_detail_weather);
+		mOccasionTextView = (TextView)findViewById(R.id.activity_suit_detail_occasion);
+		mCreateTimeTextView = (TextView)findViewById(R.id.activity_suit_detail_createTime);
+		mLastEditTextView = (TextView)findViewById(R.id.activity_suit_detail_lastEdit);
+		mIsLike = (TextView)findViewById(R.id.activity_suit_detail_isLike);
+		mImgImageView = (ImageView)findViewById(R.id.activity_suit_detail_img);
+		mClothesListView = (ListView)findViewById(R.id.activity_suit_detail_clothesList);
+		
+	}
 }
